555f6be983540f3425eb92efdbe07230af45f8e0
gz-freedom <myec2005@163.com>
2018-11-23 15:22:22 +0800

    add service

---
+++ src/app/ex.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExService {
+  uri = 'http://localhost:4000/';
+
+  constructor(private http: HttpClient) { }
+
+  getCurrentExpenditure() {
+    return this.http.get(`${this.uri}`);
+  }
+}
